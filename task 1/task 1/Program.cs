cfb6007441f56e795713bbd9635743d099642b96
Fatimə Qədirova <fatimagg@code.edu.az>
2023-06-30 22:07:44 +0400

    task 1 added

---
+++ task 1/task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace task_1
+{
+   
+        public class Person
+        {
+            public string Name { get; private set; }
+            public string Surname { get; private set; }
+            public  int Age { get; private set; }
+            public decimal Weight { get; private set; }
+            public decimal Height { get; private set; }
+            public Person (string name, string surname,  int age, decimal weight, decimal height)
+            {
+                Name = name;
+                Surname = surname;
+                Age = age;
+                Weight = weight;
+                Height = height;
+            }
+
+        public override string ToString()
+            {
+                return Name + " " + Surname + " " + Age + " " + Weight + " " + Height;
+            }
+        }
+         public  class Program
+    { 
+        static void Main(string[] args)
+        {
+            var person = new Person("Fatima", "Qadirova", 22, 47m, 1.61m);
+
+            Console.WriteLine(person);
+
+              Console.WriteLine("Enter your outward appearance: ");
+
+               string input = Console.ReadLine();
+        }
+    } 
+}
